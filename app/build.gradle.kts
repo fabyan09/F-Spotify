afe8fab28905aa7fc9a21c9c65bf5b63d84f95dc
Fabrice GERBAUD <fabricegerbaud06@gmail.com>
2025-06-03 16:19:38 +0200

    removed useless Picasso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,10 +36,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-
-    // === Picasso ===
-    implementation("com.squareup.picasso:picasso:2.71828")
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
